358ddad1b1f005c537d640e0238fa13589a6fa68
reus <reusee@gmail.com>
2024-03-29 15:28:54 +0800

    add iter benchmarks

--- iter_test.go
+++ iter_test.go
@@ -7,26 +7,8 @@ import (
 )
 
 func TestIter(t *testing.T) {
-	node := &node[Int]{
-		left: &node[Int]{
-			left: &node[Int]{
-				value: 1,
-			},
-			right: &node[Int]{
-				value: 2,
-			},
-			value: 3,
-		},
-		right: &node[Int]{
-			right: &node[Int]{
-				value: 4,
-			},
-			value: 5,
-		},
-		value: 6,
-	}
+	iter := testNode.iter(nil)
 
-	iter := node.iter(nil)
 	out := new(strings.Builder)
 	var n Int
 	for {
@@ -39,5 +21,34 @@ func TestIter(t *testing.T) {
 
 	if out.String() != "132654" {
 		t.Fatalf("got %v", out.String())
+	}
+}
+
+var testNode = &node[Int]{
+	left: &node[Int]{
+		left: &node[Int]{
+			value: 1,
+		},
+		right: &node[Int]{
+			value: 2,
+		},
+		value: 3,
+	},
+	right: &node[Int]{
+		right: &node[Int]{
+			value: 4,
+		},
+		value: 5,
+	},
+	value: 6,
+}
+
+func BenchmarkIter(b *testing.B) {
+	iter := testNode.iter(nil)
+	for i := 0; i < b.N; i++ {
+		_, iter = iter()
+		if iter == nil {
+			iter = testNode.iter(nil)
+		}
 	}
 }
