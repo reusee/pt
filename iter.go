75305e78ecbf7d95f3c9baccae14c9e51b1b1cb9
reus <reusee@gmail.com>
2024-04-04 13:35:00 +0800

    add Iter.Seek

--- iter.go
+++ iter.go
@@ -6,17 +6,56 @@ type Iter[T Ordered[T]] struct {
 }
 
 func (i *Iter[T]) Next() (ret T, ok bool) {
+	// push all left nodes to stack
 	for i.current != nil {
 		i.stack = append(i.stack, i.current)
 		i.current = i.current.left
 	}
+	// no more
 	if len(i.stack) == 0 {
 		return
 	}
+	// pop
 	node := i.stack[len(i.stack)-1]
 	i.stack = i.stack[:len(i.stack)-1]
+	// read
 	ret = node.value
 	ok = true
+	// read right node next
+	i.current = node.right
+	return
+}
+
+func (i *Iter[T]) Seek(pivot T) (ret T, ok bool) {
+	// push all left nodes to stack
+	for i.current != nil {
+		switch pivot.Compare(i.current.value) {
+		case 0:
+			// found
+			ret = i.current.value
+			ok = true
+			i.current = i.current.right
+			return
+		case 1:
+			// go right node
+			i.current = i.current.right
+		case -1:
+			// go left node
+			i.stack = append(i.stack, i.current)
+			i.current = i.current.left
+		}
+	}
+	// no more
+	if len(i.stack) == 0 {
+		return
+	}
+	// pop
+	node := i.stack[len(i.stack)-1]
+	i.stack = i.stack[:len(i.stack)-1]
+	// read
+	ret = node.value
+	ok = true
+	// read right node next
 	i.current = node.right
 	return
 }
--- iter_test.go
+++ iter_test.go
@@ -70,3 +70,67 @@ func BenchmarkParallelIter(b *testing.B) {
 		}
 	})
 }
+
+func TestSeek(t *testing.T) {
+	ps := NewPrioritySource()
+	var node *Treap[Int]
+	const num = 128
+	// insert
+	var expected []Int
+	for i := 0; i < num; i += 2 {
+		node, _ = node.Upsert(Int(i), ps(), false)
+		expected = append(expected, Int(i))
+	}
+	// seek
+	for i := 0; i < num; i++ {
+		iter := node.NewIter()
+
+		// seek
+		switch i % 2 {
+
+		case 0:
+			n, ok := iter.Seek(Int(i))
+			if !ok {
+				t.Fatal()
+			}
+			if n != Int(i) {
+				t.Fatal()
+			}
+
+		case 1:
+			n, ok := iter.Seek(Int(i))
+			if i == num-1 {
+				// last
+				if ok {
+					t.Fatal()
+				}
+			} else {
+				if !ok {
+					t.Fatal()
+				}
+				if n != Int(i+1) {
+					t.Fatal()
+				}
+			}
+
+		}
+
+		// next
+		if i != num-1 {
+			idx := (i+1)/2 + 1
+			for {
+				n, ok := iter.Next()
+				if !ok {
+					break
+				}
+				if n != expected[idx] {
+					t.Fatal()
+				}
+				idx++
+			}
+			if idx != num/2 {
+				t.Fatal()
+			}
+		}
+	}
+}
