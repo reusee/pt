b262303d2c5a5f57b733149f9874bcf1ae9f643d
reus <reusee@gmail.com>
2024-04-07 16:13:50 +0800

    add FuzzTreap

--- fuzz_test.go
+++ fuzz_test.go
@@ -8,30 +8,70 @@ import (
 func FuzzTreap(f *testing.F) {
 	f.Fuzz(func(t *testing.T, i uint64) {
 		r := rand.New(rand.NewPCG(i, i))
+		data := r.Perm(4096)
+
 		var node *Treap[Int]
-		for i := 0; i < 2048; i++ {
-			v := Int(r.Int64())
-			node, _ = node.Upsert(
-				v,
+		for _, v := range data {
+			// upsert
+			node, ok := node.Upsert(
+				Int(v),
 				r.Int64()-1,
 				false,
 			)
-			_, ok := node.Get(v)
+			if ok {
+				t.Fatal()
+			}
+
+			// upsert again
+			node, ok = node.Upsert(
+				Int(v),
+				r.Int64()-1,
+				false,
+			)
 			if !ok {
 				t.Fatal()
 			}
+
+			// get
+			_, ok = node.Get(Int(v))
+			if !ok {
+				t.Fatal()
+			}
+
+			// iter
 			iter := node.NewIter(nil)
-			value, ok := iter.Seek(v)
+			// seek
+			value, ok := iter.Seek(Int(v))
 			if !ok {
 				t.Fatal()
 			}
-			if value != v {
+			if value != Int(v) {
 				t.Fatal()
 			}
 			iter.Close()
 		}
-		if node.Length() != 2048 {
+
+		// length
+		if node.Length() != len(data) {
+			t.Fatalf("got %v, expected %v", node.Length(), len(data))
+		}
+
+		// iter all
+		var iterResult []int
+		iter := node.NewIter(nil)
+		for n, ok := iter.Next(); ok; n, ok = iter.Next() {
+			iterResult = append(iterResult, int(n))
+		}
+		if len(iterResult) != len(data) {
 			t.Fatal()
 		}
+		for i, v := range iterResult {
+			if i > 0 {
+				if v != iterResult[i] {
+					t.Fatal()
+				}
+			}
+		}
+
 	})
 }
