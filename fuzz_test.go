06406210d0aa4e2589378b278ce22550c7095572
reus <reusee@gmail.com>
2024-04-07 16:41:35 +0800

    refine FuzzTreap

--- fuzz_test.go
+++ fuzz_test.go
@@ -11,38 +11,45 @@ func FuzzTreap(f *testing.F) {
 		data := r.Perm(4096)
 
 		var node *Treap[Int]
-		for _, v := range data {
+		for i, v := range data {
 			// upsert
-			node, ok := node.Upsert(
+			var existed bool
+			node, existed = node.Upsert(
 				Int(v),
 				r.Int64()-1,
 				false,
 			)
-			if ok {
+			if existed {
 				t.Fatal()
+			}
+			if node.Length() != i+1 {
+				t.Fatalf("got %v, expected %v", node.Length(), i+1)
 			}
 
 			// upsert again
-			node, ok = node.Upsert(
+			node, existed = node.Upsert(
 				Int(v),
 				r.Int64()-1,
 				false,
 			)
-			if !ok {
+			if !existed {
+				t.Fatal()
+			}
+			if node.Length() != i+1 {
 				t.Fatal()
 			}
 
 			// get
-			_, ok = node.Get(Int(v))
-			if !ok {
+			_, existed = node.Get(Int(v))
+			if !existed {
 				t.Fatal()
 			}
 
 			// iter
 			iter := node.NewIter(nil)
 			// seek
-			value, ok := iter.Seek(Int(v))
-			if !ok {
+			value, existed := iter.Seek(Int(v))
+			if !existed {
 				t.Fatal()
 			}
 			if value != Int(v) {
@@ -62,12 +69,14 @@ func FuzzTreap(f *testing.F) {
 		for n, ok := iter.Next(); ok; n, ok = iter.Next() {
 			iterResult = append(iterResult, int(n))
 		}
+		iter.Close()
 		if len(iterResult) != len(data) {
 			t.Fatal()
 		}
+		// asc
 		for i, v := range iterResult {
 			if i > 0 {
-				if v != iterResult[i] {
+				if v != iterResult[i-1]+1 {
 					t.Fatal()
 				}
 			}
