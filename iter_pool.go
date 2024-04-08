97c4d3e135900735aa06ef407d7cbdf771d86633
reus <reusee@gmail.com>
2024-04-08 14:41:17 +0800

    copy TypeFor from the std library

--- iter_pool.go
+++ iter_pool.go
@@ -8,7 +8,7 @@ import (
 var iterPool = new(sync.Map) // reflect.Type -> *sync.Pool
 
 func getIter[T Ordered[T]]() *Iter[T] {
-	t := reflect.TypeFor[T]()
+	t := TypeFor[T]()
 	v, ok := iterPool.Load(t)
 	if ok {
 		iter := v.(*sync.Pool).Get().(*Iter[T])
@@ -28,7 +28,7 @@ func putIter[T Ordered[T]](iter *Iter[T]) {
 	iter.stack = iter.stack[:0]
 	iter.root = nil
 	iter.next = nil
-	t := reflect.TypeFor[T]()
+	t := TypeFor[T]()
 	v, ok := iterPool.Load(t)
 	if ok {
 		v.(*sync.Pool).Put(iter)
@@ -41,4 +41,14 @@ func putIter[T Ordered[T]](iter *Iter[T]) {
 	})
 	pool := v.(*sync.Pool)
 	pool.Put(iter)
+}
+
+// TypeFor returns the [Type] that represents the type argument T.
+// copy from the std library
+func TypeFor[T any]() reflect.Type {
+	var v T
+	if t := reflect.TypeOf(v); t != nil {
+		return t // optimize for T being a non-interface kind
+	}
+	return reflect.TypeOf((*T)(nil)).Elem() // only for an interface kind
 }
