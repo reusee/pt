6d1ab03a78ca5137b63b34b61c58a4cb98e65b6a
reus <reusee@gmail.com>
2024-04-10 10:11:14 +0800

    make default priority source thread-safe

--- priority.go
+++ priority.go
@@ -5,6 +5,8 @@ import (
 	"encoding/binary"
 	"math"
 	"math/rand"
+	"runtime"
+	_ "unsafe"
 )
 
 type _Priority = int64
@@ -17,14 +19,28 @@ const (
 type _PrioritySource = func() _Priority
 
 func newPrioritySource() _PrioritySource {
-	var s1 int64
-	if err := binary.Read(crand.Reader, binary.LittleEndian, &s1); err != nil {
-		panic(err)
+	var shards []*rand.Rand
+	numShards := runtime.GOMAXPROCS(-1)
+	for i := 0; i < numShards; i++ {
+		var s1 int64
+		if err := binary.Read(crand.Reader, binary.LittleEndian, &s1); err != nil {
+			panic(err)
+		}
+		r := rand.New(
+			rand.NewSource(s1),
+		)
+		shards = append(shards, r)
 	}
-	r := rand.New(
-		rand.NewSource(s1),
-	)
 	return func() _Priority {
-		return r.Int63() - 1 // minus one to avoid returning the max int64
+		proc := runtime_procPin()
+		shard := shards[proc]
+		runtime_procUnpin()
+		return shard.Int63() - 1 // minus one to avoid returning the max int64
 	}
 }
+
+//go:linkname runtime_procPin runtime.procPin
+func runtime_procPin() int
+
+//go:linkname runtime_procUnpin runtime.procUnpin
+func runtime_procUnpin() int
