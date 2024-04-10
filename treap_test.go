b352e824467b76dba0a9e52d638dd2a16ce4866e
reus <reusee@gmail.com>
2024-04-10 10:28:59 +0800

    more benchmarks

--- treap_test.go
+++ treap_test.go
@@ -2,7 +2,7 @@ package pt
 
 import "testing"
 
-func BenchmarkTreapParallelUpsert(b *testing.B) {
+func BenchmarkTreapParallelUpsertSeries(b *testing.B) {
 	treap := NewTreap[Int]()
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
@@ -12,12 +12,43 @@ func BenchmarkTreapParallelUpsert(b *testing.B) {
 	})
 }
 
-func BenchmarkTreapParallelBulkUpsert(b *testing.B) {
+func BenchmarkTreapParallelUpsertRandom(b *testing.B) {
+	treap := NewTreap[Int]()
+	ps := newPrioritySource()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			treap.Upsert(Int(ps()))
+		}
+	})
+}
+
+func BenchmarkTreapParallelBulkUpsertSeries(b *testing.B) {
 	treap := NewTreap[Int]()
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for i := Int(0); pb.Next(); i++ {
 			treap.BulkUpsert([]Int{i, i + 1, i + 2, i + 3})
+		}
+	})
+}
+
+func BenchmarkTreapParallelBulkUpsertRandom(b *testing.B) {
+	treap := NewTreap[Int]()
+	ps := newPrioritySource()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			treap.BulkUpsert([]Int{
+				Int(ps()),
+				Int(ps()),
+				Int(ps()),
+				Int(ps()),
+				Int(ps()),
+				Int(ps()),
+				Int(ps()),
+				Int(ps()),
+			})
 		}
 	})
 }
